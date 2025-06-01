e9e40793796b3b9d97bb82d00583d6e30673cfdb
Sahil <sk4386840@gmail.com>
2025-06-01 11:02:01 +0530

    created to-do logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,29 @@ export default function App() {
   const [input, setInput] = useState(""); // input field state
   const [todos, setTodos] = useState([]); // list of todos
 
+  const addTodo = () => {
+    if(input.trim() === "") return;
+    setTodos([...todos, {id: Date.now(), text: input, completed: false}]);
+    setInput("");
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-lg mx-auto flex flex-col text-center
       ">
         <h1 className="max-sm:text-3xl font-semibold">Todo App</h1>
         <div className="mt-6">
-          <input className="p-3 border border-gray-900" type="text" placeholder="Enter a task" />
-          <button>Add</button>
+          <input className="p-2 rounded-lg border" type="text" placeholder="Enter a task"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={addTodo} className="p-2 rounded-lg bg-blue-500 text-white">Add</button>
         </div>
+        <ul>{
+          todos.map((todo) => { 
+           return <li id={todo.id} className="border-b py-2">{todo.text}</li>
+          })
+          }</ul>
       </div>
     </div>
   );
